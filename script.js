91298a7db229681db3e3705eb505ee29171994a7
MeBo6 <gmerabishvili1236@gmail.com>
2024-02-27 21:59:19 +0400

    JS file

---
+++ script.js
@@ -0,0 +1,62 @@
+let array = [1, 2, 3, 4, 5, 6];
+
+
+      // Array Methods
+// 1. Odd
+// for(x of array){
+//       if(x % 2 === 1){
+//            console.log(x);
+//       }
+// }
+
+// 2. Doubled numbers
+// let doubledChars = array.map(x = x*2);
+
+// 3. length of array
+// console.log(array.length);
+
+// 4. every element
+// array.forEach(x => {
+//    console.log(x);
+// });
+
+
+let text = 'random text';
+
+
+      // String Methods
+// 1. length of string with function
+// const fun = text => text.length;
+// console.log(fun(text));
+
+// 2. string in upper case
+// const fun = text => text.toUpperCase();
+// console.log(fun(text));
+
+// 3. first character of string
+// const fun = text => text.charAt(0);
+// console.log(fun(text));
+
+// 4. index of 'd'
+// const fun = text => text.indexOf('d');
+// console.log(fun(text));
+
+// 5. log string two times
+// const fun = text => text.repeat(2);
+// console.log(fun(text));
+
+// 6. log string without first three characters
+// const fun = text => text.substring(3);
+// console.log(fun(text));
+
+// 7. check if array starts with 'r'
+// const fun = text => text.startsWith('r');
+// console.log(fun(text));
+
+// 8. convert string to array
+// const fun = text => text.split(' ');
+// console.log(fun(text));
+
+// 9. replace word 'random' with 'new'
+// const fun = text => text.replace('random', 'next')
+// console.log(fun(text));
